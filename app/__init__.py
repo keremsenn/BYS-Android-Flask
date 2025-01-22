cb31f9478dcdbd63a5d584a93b8c78f45c602b0a
keremsenn <senkerem949@gmail.com>
2025-01-22 22:23:12 +0300

    app created

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,20 @@
 from flask import Flask
+from flask_migrate import Migrate
 from config import Config
-from .model.db import db
+from flask_sqlalchemy import SQLAlchemy
+from app.model import *
 
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
     db.init_app(app)
+    migrate.init_app(app, db)
+
+    with app.app_context():
+        from .model import studentCourseSelection,course,student,UserStudent,UserAdvisor,advisor,transcript
 
     return app
