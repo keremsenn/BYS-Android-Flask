8d70a5b910e03b21d52a8305f09f2c3f53bff9e1
keremsenn <senkerem949@gmail.com>
2025-03-10 17:25:44 +0300

    NavScreen oluştuurldu

---
+++ client/BYSmobil/app/src/main/java/com/keremsen/bysmobil/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.keremsen.bysmobil
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.keremsen.bysmobil.navigation.NavScreen
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NavScreen()
+        }
+    }
+
+}
---
+++ client/BYSmobil/app/src/main/java/com/keremsen/bysmobil/navigation/NavScreen.kt
@@ -0,0 +1,36 @@
+package com.keremsen.bysmobil.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.keremsen.bysmobil.view.LoginPage
+import com.keremsen.bysmobil.view.MainPage
+
+
+@Composable
+fun NavScreen() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "loginPage") {
+        composable(
+            "main/{is_student}/{studentId}/{advisorId}",
+            arguments = listOf(
+                navArgument("is_student") { type = NavType.StringType },
+                navArgument("studentId"){ type = NavType.IntType },
+                navArgument("advisorId"){type=NavType.IntType}
+            )
+        ) { backStackEntry ->
+            val isStudent = backStackEntry.arguments?.getString("is_student").toBoolean()
+            val studentId = backStackEntry.arguments?.getInt("studentId")!!.toInt()
+            val advisorId = backStackEntry.arguments?.getInt("advisorId")!!.toInt()
+            MainPage(navController, isStudent, studentId,advisorId)
+        }
+        composable("loginPage") {
+            LoginPage(navController)
+        }
+    }
+
+}
